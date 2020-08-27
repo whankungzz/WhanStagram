e04a5ee5f2dcd5edbf19639155d44668c3a97c0b
tammaporn <sweet.whankungz@hotmail.com>
2020-08-27 16:06:36 +0700

    add api

--- whanstagram/src/components/GridImages.vue
+++ whanstagram/src/components/GridImages.vue
@@ -7,7 +7,7 @@
                 <b>{{ username }}</b> 
             </div>
             <div class="image">
-                <img :src="avatarImg">
+                <img :src="apis.data.message[i]">
             </div>
             <div class="content">
                 <span class="right floated">
@@ -24,19 +24,28 @@
                 </div>
             </div>
         </div>
+
+        {{ apis.data.message }}
     </div>
     
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name: 'GridImages',
     data: function() {
         return {
             avatarImg: 'https://tse3.mm.bing.net/th?id=OIP.nFyCAO1Ml4NC8PbOVi9uVQHaGr&pid=Api&P=0&w=193&h=175',
-
-            usernames: ['whan1', 'whan2', 'whan3', 'whan4', 'whan5']
+            usernames: ['whan1', 'whan2', 'whan3', 'whan4', 'whan5'],
+            apis: null
         }
+    },
+    mounted: function() {
+        axios
+        .get('https://dog.ceo/api/breed/pomeranian/images/random/10')
+        .then(response => this.apis = response)
     }
 }
 </script>
